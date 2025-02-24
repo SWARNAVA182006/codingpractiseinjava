e7e0d9d4e1dcedc97e7770fff347c7469d480a99
SWARNAVA SARKAR <swarnava2019@gmail.com>
2025-02-25 02:43:39 +0530

    Add files via upload
    distance among cities

---
+++ distance.java
@@ -0,0 +1,7 @@
+class distance{
+public static void main(String[] args){
+double d_km=10.8;//distance given in km
+double d_miles=(d_km*1.6);//converting distance from km to miles
+System.out.println("The distance 10.8 km in miles is= "+ d_miles);
+}
+}
