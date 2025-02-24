34d28608aee55a51c1de3b332ec7e8898d3aa926
SWARNAVA SARKAR <swarnava2019@gmail.com>
2025-02-25 02:54:36 +0530

    Add files via upload
    pens

---
+++ Pens.java
@@ -0,0 +1,8 @@
+class Pens{
+public static void main(String[]args){
+int pens=14;//no. of pens
+int students=3;//no. of students
+int dis=pens/students;//distributing the pens among 3
+int rem_pen=pens%students;//remaining pen numbers
+System.out.println("The Pen Per Student is "+dis+" and the remaining pen not distributed is "+rem_pen);
+}}
