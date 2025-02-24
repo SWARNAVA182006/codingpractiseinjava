2a12df86cf06da5bc0abd7471ece3e3e05f4ef2b
SWARNAVA SARKAR <swarnava2019@gmail.com>
2025-02-25 02:55:40 +0530

    Add files via upload
    profit calculation

---
+++ profit.java
@@ -0,0 +1,9 @@
+class profit{
+public static void main(String[]args){
+float cost_price=129,selling_price=191;//putting cost and selling price
+float profit=selling_price-cost_price;//calculating profit
+float profit_percentage=(profit/cost_price)*100;//profit in percentage
+System.out.println("The Cost Price is INR "+cost_price+ "and Selling Price is INR "+selling_price+"\n");
+System.out.println("The Profit is INR "+profit+" and the Profit Percentage is "+profit_percentage);
+}
+}
